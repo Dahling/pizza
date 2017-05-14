693f23d50b2ee8ff18223fd1cc478560fe6e0e68
Jenna Cooper <jchristinec@gmail.com>
2017-05-14 11:58:00 -0700

    Add some details to the README file, changed a little CSS, added image at bottom

---
+++ README.md
@@ -0,0 +1,48 @@
+# _Pizza Company Website_
+
+#### _Week 4 Project, 5.12.2017_
+
+#### By _**Jenna Cooper**_
+
+## Description
+
+_This is a play-pretend pizza company website, where a user can order a customized pizza by picking their preferences. Preferences include: different-sized pizzas, and all sort of toppings. The purpose of this project is to create object constructors with properties, using object-oriented Javascript._
+
+
+## Specs
+
+| Objective | Example: Input | Example: Output |
+|:-------------:|:-------------:|:-------------:|
+| Output can determine pizza sizes | small | small |
+| Output includes whichever toppings are chosen |  Organic Tomato Sauce |  Organic Tomato Sauce|
+| Output determines price of pizza by adding the price of toppings to initial price |  Organic Tomato Sauce |  +=1 |
+| Output determines price of pizza by adding the price of the size of pizza | small | +=10 |
+| Output includes customer's name, size of pizza, ingredients, and the subtotal of the pizza | Jenna, Mini, Garlic | Jenna, Mini, Garlic, $21  |
+
+
+## Setup/Installation Requirements
+
+* _Clone this Repository onto your Desktop, via the "Clone or Download" button on GitHub._
+* _Once downloaded, click the index.html._
+* _Open it up, via your favorite browser (...I like Firefox, but apparently it's not one of the cool kids...)._
+* _Open any of the files up, via a program like Atom, in order to see the code or play around with it._
+* _Have a lovely day_
+
+
+## Known Bugs
+
+I'm not sure there's any sort of bug; but, if there is, I hope it's a ladybug.
+
+## Support and contact details
+
+_If you have any issues, email me, and I will gladly respond._
+
+## Technologies Used
+
+_I used Bootstrap to create this responsive layout, as well as many of Bootstrap's features (e.g., classes, etc.). Other than that, I used HTML, CSS, and Javascript. I also used the Terminal, Git, and GitHub. This was all done on my personal MacBook Air._
+
+### License
+
+*MIT License*
+
+Copyright (c) 2017 **_Jenna Cooper_**
--- js/scripts.js
+++ js/scripts.js
@@ -11,18 +11,18 @@ function Customer(name, address) {
   this.name = name;
 }
 
-// Price Prototype 
+// Price Prototype
 Pizza.prototype.price = function() {
   var price = 20;
 
   if (this.pizzasize === "Large") {
-    price += 10;
+    price += 15;
   } else if (this.pizzasize === "Medium") {
-    price += 15;
+    price += 10;
   } else if (this.pizzasize === "Small") {
-    price += 15;
-  } else {
     price += 5;
+  } else {
+    price += 0;
   }
 
   if (this.pizzaingredients.length === 0) {
@@ -34,8 +34,8 @@ Pizza.prototype.price = function() {
   return price;
 };
 
-// Front-End User Logic
-$(document).ready(function() {
+// Front-End Logic
+  $(document).ready(function() {
   $("form#pizza").submit(function(event) {
     event.preventDefault();
     var inputtedName = $("#name").val();
@@ -43,18 +43,18 @@ $(document).ready(function() {
     var newCustomer = new Customer(inputtedName);
     var newPizza = new Pizza(inputtedPizzaSize);
 
-    $.each($("input[name='toppings']:checked"), function() {
-      newPizza.pizzaingredients.push($(this).val());
-    });
+  $.each($("input[name='toppings']:checked"), function() {
+    newPizza.pizzaingredients.push($(this).val());
+  });
 
-    $("ul#pizza-order-list").append("<li><button type='submit' class='btn '><span class='pizzaOrder'>" + "Verify Order" + "</button></span></li>");
+  $("ul#pizza-order-list").append("<li><button type='submit' class='btn '><span class='pizzaOrder'>" + "Verify Order" + "</button></span></li>");
 
-    $(".pizzaOrder").last().click(function() {
-      $("#pizza-order-detail").show();
-      $(".name").text(newCustomer.name);
-      $(".pizza-size").text(newPizza.pizzasize);
-      $(".pizza-ingredients").text(newPizza.pizzaingredients);
-      $(".order-total").text(newPizza.price());
+  $(".pizzaOrder").last().click(function() {
+    $("#pizza-order-detail").show();
+    $(".name").text(newCustomer.name);
+    $(".pizza-size").text(newPizza.pizzasize);
+    $(".pizza-ingredients").text(newPizza.pizzaingredients);
+    $(".order-total").text(newPizza.price());
 
     });
   });
