a659f7a19a9ad57fc373701fadbaef720f5dd8c4
Jenna Cooper <jchristinec@gmail.com>
2017-05-14 22:37:28 -0700

    Add some explanations to js, going through what is occurring

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,24 @@
 // Business Logic
 
 // Pizza Constructor
-function Pizza(size) {
+function Pizza(size, ingredients) {
   this.pizzasize = size;
   this.pizzaingredients = [];
 };
+/*'this.' applies to every pizza; thus, this.pizzasize applies to every pizza's individual size, and this.pizzaingredients applies to every pizza's individual ingredients - an open array*/
+
 
 // Customer Constructor
 function Customer(name) {
   this.name = name;
 }
+//'this.' applies to every customer; thus, this.name applies to every customer's individual name
 
 // Price Prototype
+/* creating a Pizza prototype creates a shared prototype in which a method, like .price, can be applied to every new Pizza*/
 Pizza.prototype.price = function() {
   var price = 20;
-
+/*this creates a variable named price, saying the price is 20; below, there is if and else if, showing that if this (new Pizza) pizza size is strictly equal to so-and-so size, the price is += a certain amount, else (aka if it's a mini), there is no additional charge).*/
   if (this.pizzasize === "Large Snobby Pizza") {
     price += 15;
   } else if (this.pizzasize === "Medium Snobby Pizza") {
@@ -24,7 +28,7 @@ Pizza.prototype.price = function() {
   } else {
     price += 0;
   }
-
+/*the same occurs, below, for what occurred above, except it addresses this.pizzaingredients.length, as far as the open array in the function, at the very top. */
   if (this.pizzaingredients.length === 0) {
     price += 0;
   } else {
